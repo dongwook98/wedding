6276b12b6d4f06214330e6664a9cf329984e9726
강동욱 <ehddnr8563@gmail.com>
2025-02-12 17:05:03 +0900

    feat: 캘린더 구현

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Video from '@components/sections/Video';
 import ImageGallery from '@components/sections/ImageGallery';
 import Intro from '@components/sections/Intro';
 import Invitation from './components/sections/Invitation';
+import Calendar from './components/sections/Calendar';
 
 const cx = classNames.bind(styles);
 
@@ -68,6 +69,7 @@ function App() {
         message={message.intro}
       />
       <Invitation message={message.invitation} />
+      <Calendar date={date} />
       {JSON.stringify(wedding)}
     </div>
   );
---
+++ src/components/sections/Calendar.tsx
@@ -0,0 +1,65 @@
+import classNames from 'classnames/bind';
+import { parseISO, format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { DayPicker } from 'react-day-picker';
+
+import 'react-day-picker/style.css';
+import styles from './Calendar.module.scss';
+import Section from '../shared/Section';
+
+const cx = classNames.bind(styles);
+
+const css = `
+  .rdp-nav {
+    display: none;
+  }
+  .rdp-month_caption {
+    display: none;
+  }
+  .rdp-weekdays {
+    font-size: 30px;
+  }
+  .rdp-weekday {
+    font-weight: bold;
+  }
+  .rdp-day .rdp-day_button {
+    cursor: default;
+  }
+  .rdp-selected .rdp-day_button {
+    border: none;
+    color: white;
+    font-weight: bold;
+    background: var(--red);
+  }
+  .rdp-selected .rdp-day_button:hover {
+    background: var(--red);
+  }
+`;
+
+export default function Calendar({ date }: { date: string }) {
+  const weddingDate = parseISO(date);
+  return (
+    <Section>
+      <div className={cx('wrap-header')}>
+        <span className={cx('txt-date')}>
+          {format(weddingDate, 'yyyy.MM.dd')}
+        </span>
+        <span className={cx('txt-time')}>
+          {format(weddingDate, 'aaa h시 eeee', { locale: ko })}
+        </span>
+      </div>
+
+      <div className={cx('wrap-daypicker')}>
+        <style>{css}</style>
+        <DayPicker
+          mode='single'
+          locale={ko}
+          month={weddingDate}
+          selected={weddingDate}
+          onSelect={() => {}}
+          formatters={{ formatCaption: () => '' }}
+        />
+      </div>
+    </Section>
+  );
+}
